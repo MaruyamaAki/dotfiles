aae6d74af7e73b25c53a5d42ee77602cbccab325
MaruayamaAki <maruyama@engineerforce.io>
2025-07-16 11:18:53 +0900

    remove .config

--- .config/fish/functions/ghq.fish
+++
@@ -1,8 +0,0 @@
-function ghq
-    if [ $argv[1] = repo ]
-        set --erase argv[1]
-        source ~/.config/fish/functions/ghq.sh $argv 2>/dev/null
-    else
-        command ghq $argv
-    end
-end
